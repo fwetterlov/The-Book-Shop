a219f14f5f832b9bca5300e69b4a52584e758eb5
fwett <fwetterlov@gmail.com>
2023-03-04 08:32:44 +0100

    added tableview, quantity and total to shopcart

--- main.js
+++ main.js
@@ -48,6 +48,92 @@ function getMatchingArray(searchTerm) {
   }
 }
 
+function calculateTotalPrice(cart) {
+  const totalPrice = cart.reduce((acc, book) => acc + book.price, 0);
+  return totalPrice;
+}
+
+function getBookDetails(shopCart) {
+  const bookDetails = {};
+  for (let i = 0; i < shopCart.length; i++) {
+    const book = shopCart[i];
+    if (!bookDetails[book.title]) {
+      bookDetails[book.title] = {
+        title: book.title,
+        price: book.price,
+        quantity: 1
+      };
+    } else {
+      bookDetails[book.title].quantity++;
+    }
+  }
+  return Object.values(bookDetails);
+}
+
+function displayShoppingCart() {
+  const totalPrice = calculateTotalPrice(shopCart);
+  const bookDetails = getBookDetails(shopCart);
+  let shopCartHtml = "";
+  shopCartHtml += /*html*/`
+    <div class="modal" id="modal-content">
+      <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close" data-mdb-ripple-color="dark"></button>
+      <div class="container">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+      `;
+
+  // Loop through the books in shopCart and create a row for each one
+  for (let i = 0; i < bookDetails.length; i++) {
+    let book = bookDetails[i];
+    shopCartHtml += /*html*/`
+        <tr>
+          <td>${book.title}</td>
+          <td>${book.price}</td>
+          <td>${book.quantity}</td>
+          <td>
+            <button type="button" id="remove-button" class="btn btn-light remove-button" data-mdb-ripple-color="dark">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/></svg>
+            </button>
+          </td>
+        </tr>
+      `;
+  }
+
+  // Close the table element
+  shopCartHtml += /*html*/`
+          </tbody>
+        </table>
+        <div class="checkout-container">
+          <p style="font-size: 17px; color: grey;">Total: ${totalPrice}kr
+          <button type="button" class="btn btn-light checkout-button" data-mdb-ripple-color="dark">Checkout</button>
+        </div>
+      </div>
+    `;
+
+  //document.body.insertAdjacentHTML('beforeend', detailsHtml);
+  document.querySelector('.modal-container').innerHTML = shopCartHtml;
+
+  const closeButton = document.querySelector('.btn-close');
+  closeButton.addEventListener('click', function (event) {
+    document.querySelector('.modal-container').innerHTML = "";
+  });
+
+  const removeButtons = document.querySelectorAll('#remove-button');
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', function (event) {
+      console.log("quantity--;");
+    });
+  }
+}
+
 function addFiltersAndSort() {
   let filtersHtml = /*html*/`
   <select class="filters">
@@ -101,45 +187,7 @@ function addFiltersAndSort() {
   const shoppingCartButton = document.getElementById("shoppingcart");
   shoppingCartButton.addEventListener("click", function () {
     console.log(shopCart);
-    let shopCartHtml = "";
-    shopCartHtml += /*html*/`
-    <div class="modal" id="modal-content">
-      <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close" data-mdb-ripple-color="dark"></button>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-    `;
-
-    // Loop through the books in shopCart and create a row for each one
-    for (let i = 0; i < shopCart.length; i++) {
-      let book = shopCart[i];
-      shopCartHtml += /*html*/`
-      <tr>
-        <td>${book.title}</td>
-        <td>${book.price}</td>
-      </tr>
-    `;
-    }
-
-    // Close the table element
-    shopCartHtml += /*html*/`
-        </tbody>
-      </table>
-    </div>
-  `;
-
-    //document.body.insertAdjacentHTML('beforeend', detailsHtml);
-    document.querySelector('.modal-container').innerHTML = shopCartHtml;
-
-    const closeButton = document.querySelector('.btn-close');
-    closeButton.addEventListener('click', function (event) {
-      document.querySelector('.modal-container').innerHTML = "";
-    });
+    displayShoppingCart();
   });
 }
 
